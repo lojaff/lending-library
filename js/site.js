930b4fc788249db1d6dd828089f276a435bdf103
Logan Jaffe <loganjaffe@gmail.com>
2014-08-12 14:01:48 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -69,23 +69,4 @@ function filterTools(text) {
   })
 }
 
-$(function(){
-  $(".increment").click(function(){
-    var count = parseInt($("~ .count", this).text());
-    
-    if($(this).hasClass("up")) {
-      var count = count + 1;
-      
-       $("~ .count", this).text(count);
-    } else {
-      var count = count - 1;
-       $("~ .count", this).text(count);     
-    }
-    
-    $(this).parent().addClass("bump");
-    
-    setTimeout(function(){
-      $(this).parent().removeClass("bump");    
-    }, 400);
-  });
-});
+
